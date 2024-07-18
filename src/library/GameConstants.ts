e4fcb214a79b2576e44580ac09c571abeeeb7056
Christian Hart <christiantaylorhart@gmail.com>
2024-07-18 10:59:58 -0400

    Create easyLevel in GameConstants.ts

--- src/library/GameConstants.ts
+++ src/library/GameConstants.ts
@@ -1,3 +1,57 @@
+export const easyLevel = [
+  {
+    sceneBoundary: 19,
+    characterCount: 25,
+    groundSprite: 'ground_tile1.png',
+    characterSprites: [
+      'characterSprites/blue/ff-diagonal_blue.png',
+      'characterSprites/red/ff-diagonal_flip_red.png',
+    ],
+    frankieSprites: [
+      'characterSprites/green/ff-polka_green.png',
+      'characterSprite/green/ff-polka_green-cheer.png',
+      'characterSprite/sweater/ff-polka_green-sweater.png',
+    ],
+    cheerSprite: 'characterSprite/green/ff-polka_green-cheer.png',
+  },
+  {
+    sceneBoundary: 19,
+    characterCount: 50,
+    groundSprite: 'ground_tile1.png',
+    characterSprites: [
+      'characterSprites/blue/ff-diagonal_blue.png',
+      'characterSprites/blue/ff-wavy_blue.png',
+      'characterSprites/red/ff-diagonal_flip_red.png',
+      'characterSprites/red/ff-wavy_flip_red.png',
+    ],
+    frankieSprites: [
+      'characterSprites/green/ff-polka_green.png',
+      'characterSprite/green/ff-polka_green-cheer.png',
+      'characterSprite/sweater/ff-polka_green-sweater.png',
+    ],
+    cheerSprite: 'characterSprite/green/ff-polka_green-cheer.png',
+  },
+  {
+    sceneBoundary: 19,
+    characterCount: 100,
+    groundSprite: 'ground_tile1.png',
+    characterSprites: [
+      'characterSprites/blue/ff-diagonal_blue.png',
+      'characterSprites/blue/ff-wavy_blue.png',
+      'characterSprites/blue/ff-polka_blue.png',
+      'characterSprites/red/ff-diagonal_flip_red.png',
+      'characterSprites/red/ff-wavy_flip_red.png',
+      'characterSprites/red/ff-polka_red.png',
+    ],
+    frankieSprites: [
+      'characterSprites/green/ff-polka_green.png',
+      'characterSprite/green/ff-polka_green-cheer.png',
+      'characterSprite/sweater/ff-polka_green-sweater.png',
+    ],
+    cheerSprite: 'characterSprite/green/ff-polka_green-cheer.png',
+  },
+];
+
 export const characterColors = [
   0x9b5de5, 0x8c54ce, 0x7c4ab7, 0x6d41a0, 0xa56de8, 0xaf7dea,
 ];
@@ -27,28 +81,3 @@ export const frankieSprite = ['characterSprites/green/ff-polka_green.png'];
 export const cheerSprite = 'characterSprites/green/ff-polka_green-cheer.png';
 
 export const groundSprite = 'ground_tile1.png';
-
-export const level = [
-  {
-    sceneBoundary: 19,
-    characterCount: 500,
-    groundSprite: 'ground_tile1.png',
-    characterSprites: [
-      'characterSprites/blue/ff-diagonal_blue.png',
-      'characterSprites/blue/ff-diagonal_flip_blue.png',
-      'characterSprites/blue/ff-polka_blue.png',
-      'characterSprites/blue/ff-polka_flip_blue.png',
-      'characterSprites/blue/ff-wavy_blue.png',
-      'characterSprites/blue/ff-wavy_flip_blue.png',
-      'characterSprites/red/ff-diagonal_red.png',
-      'characterSprites/red/ff-diagonal_flip_red.png',
-      'characterSprites/red/ff-polka_red.png',
-      'characterSprites/red/ff-polka_flip_red.png',
-      'characterSprites/red/ff-wavy_red.png',
-      'characterSprites/red/ff-wavy_flip_red.png',
-      'characterSprites/red/ff-polka_red.png',
-    ],
-    frankieSprite: ['characterSprites/green/ff-polka_green.png'],
-    cheerSprite: 'characterSprite/green/ff-polka_green-cheer.png',
-  },
-];

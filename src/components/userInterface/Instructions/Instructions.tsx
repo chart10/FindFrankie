7934945fd08f515b3a431b528147505093c70542
Christian Hart <christiantaylorhart@gmail.com>
2024-07-20 11:27:03 -0400

    Improve Instructions CSS styles

--- src/components/userInterface/Instructions/Instructions.tsx
+++ src/components/userInterface/Instructions/Instructions.tsx
@@ -3,24 +3,36 @@ import { frankieSprites } from '../../../library/GameConstants';
 
 const Instructions = () => {
   return (
-    <div className='instructions-container'>
-      <div id='instructions' className='ui-title-card instructions-main'>
-        <div className='instructions-text'>
-          <p>Click on Frankie in the crowd below!</p>
-          <p>Frankie's sweater looks like this:</p>
+    <>
+      <div className='instructions-container'>
+        <div className='ui-card ui-sub-card' id='level-counter'>
+          <p>LV 1</p>
         </div>
-        <img
-          className='frankie-preview'
-          src={frankieSprites[2]}
-          height={'50px'}
-          alt='frankie'
-        />
+        <div id='instructions' className='ui-title-card instructions-main'>
+          <div className='instructions-text'>
+            <p>Click on Frankie in the crowd below!</p>
+            <p>Frankie's sweater looks like this:</p>
+          </div>
+          <img
+            className='frankie-preview'
+            src={frankieSprites[2]}
+            height={'50px'}
+            alt='frankie'
+          />
+        </div>
+        <div className='ui-card ui-sub-card' id='back-button'>
+          <p>Back</p>
+        </div>
       </div>
-      {/* <div className='ui-card ui-sub-card instructions-sub'>
-        <p>Click and drag to rock their world!</p>
-        <p>Scoll to zoom in and out!</p>
-      </div> */}
-    </div>
+      <div className='controls-container'>
+        <div className='ui-card ui-sub-card controls-card'>
+          <p>Click and drag to rock their world!</p>
+        </div>
+        <div className='ui-card ui-sub-card controls-card'>
+          <p>Scroll to zoom in and out!</p>
+        </div>
+      </div>
+    </>
   );
 };
 export default Instructions;

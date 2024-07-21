25629118e7a0e96bd8738c061eac8f481ff5ed11
Christian Hart <christiantaylorhart@gmail.com>
2024-07-21 10:15:49 -0400

    Rollback some CSS style props

--- src/components/userInterface/Instructions/Instructions.tsx
+++ src/components/userInterface/Instructions/Instructions.tsx
@@ -15,10 +15,10 @@ interface props {
 const Instructions: React.FC<props> = ({ level, gameConstants }) => {
   return (
     <>
+      <div className='ui-card ui-sub-card' id='level-counter'>
+        <p>LV {level.currentLevel + 1}</p>
+      </div>
       <div className='instructions-container'>
-        <div className='ui-card ui-sub-card' id='level-counter'>
-          <p>LV {level.currentLevel + 1}</p>
-        </div>
         <div id='instructions' className='ui-title-card instructions-main'>
           <div className='instructions-text'>
             <p>Click on Frankie in the crowd below!</p>
@@ -31,9 +31,9 @@ const Instructions: React.FC<props> = ({ level, gameConstants }) => {
             alt='frankie'
           />
         </div>
-        <div className='ui-card ui-sub-card' id='back-button'>
-          <p>Back</p>
-        </div>
+      </div>
+      <div className='ui-card ui-sub-card' id='back-button'>
+        <p>Back</p>
       </div>
       <div className='controls-container'>
         <div className='ui-card ui-sub-card controls-card'>

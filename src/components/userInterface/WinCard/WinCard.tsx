8abcd57074b24d314ba964a61e7dfb2a4a31fbd3
Christian Hart <christiantaylorhart@gmail.com>
2024-07-24 15:09:29 -0400

    WinCard's Next Level button matches mode color, Remove React import in main.tsx

--- src/components/userInterface/WinCard/WinCard.tsx
+++ src/components/userInterface/WinCard/WinCard.tsx
@@ -78,7 +78,11 @@ const WinCard: React.FC<props> = ({
             </div>
           </>
         ) : (
-          <div className='ui-card ui-sub-card' onClick={advanceToNextLevel}>
+          <div
+            className='ui-card ui-sub-card'
+            onClick={advanceToNextLevel}
+            style={{ background: `${gameConstants.color}` }}
+          >
             <p>Next Level</p>
           </div>
         )}

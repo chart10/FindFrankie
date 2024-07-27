32199728296e3f547154abd74d8bd9c95c6c63f8
Christian Hart <christiantaylorhart@gmail.com>
2024-07-27 15:44:33 -0400

    Fix game-title layout

--- src/components/userInterface/TitleCard.tsx
+++ src/components/userInterface/TitleCard.tsx
@@ -1,21 +1,40 @@
 const TitleCard = () => {
   return (
-    <div className='ui-title-card game-title'>
-      <h1>
-        <span className='letter'>F</span>
-        <span className='letter'>i</span>
-        <span className='letter'>n</span>
-        <span className='letter'>d</span>
-        &nbsp;
-        <span className='letter'>F</span>
-        <span className='letter'>r</span>
-        <span className='letter'>a</span>
-        <span className='letter'>n</span>
-        <span className='letter'>k</span>
-        <span className='letter'>i</span>
-        <span className='letter'>e</span>
-      </h1>
-    </div>
+    <>
+      <div className='ui-title-card game-title' id='game-title-main'>
+        <h1>
+          <span className='letter'>F</span>
+          <span className='letter'>i</span>
+          <span className='letter'>n</span>
+          <span className='letter'>d</span>
+          &nbsp;
+          <span className='letter'>F</span>
+          <span className='letter'>r</span>
+          <span className='letter'>a</span>
+          <span className='letter'>n</span>
+          <span className='letter'>k</span>
+          <span className='letter'>i</span>
+          <span className='letter'>e</span>
+        </h1>
+      </div>
+      <div className='ui-title-card game-title' id='game-title-alt'>
+        <div>
+          <span className='letter'>F</span>
+          <span className='letter'>i</span>
+          <span className='letter'>n</span>
+          <span className='letter'>d</span>
+        </div>
+        <div>
+          <span className='letter'>F</span>
+          <span className='letter'>r</span>
+          <span className='letter'>a</span>
+          <span className='letter'>n</span>
+          <span className='letter'>k</span>
+          <span className='letter'>i</span>
+          <span className='letter'>e</span>
+        </div>
+      </div>
+    </>
   );
 };
 export default TitleCard;
